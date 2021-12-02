293ef4d549d2fc3d287233787f8f4699992d9338
Dominik Dóczi <doczidominik@gmail.com>
2021-12-02 09:41:51 +0100

    Félbehagyott Navigation

--- Desktop/src/Vizsgaremek/Vizsgaremek/Pages/WelcomePage.xaml.cs
+++ Desktop/src/Vizsgaremek/Vizsgaremek/Pages/WelcomePage.xaml.cs
@@ -12,20 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Vizsgaremek.Navigation;
 
-namespace Vizsgaremek
+namespace Vizsgaremek.Pages
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for WelcomePage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class WelcomePage : UserControl
     {
-        public MainWindow()
+        public WelcomePage()
         {
             InitializeComponent();
-
-            
         }
     }
 }
